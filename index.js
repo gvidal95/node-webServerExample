04c89d1b69adcd198bbe69020f2f47d55412c461
gvidal <gabrielwv1095@gmail.com>
2021-12-02 17:43:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+// const path = require('path');
+
+const express = require('express');
+
+
+//Crear el servidor express
+const app = express();
+
+
+//Lectura y parseo del body
+app.use(express.json());
+
+
+//Diretorio public
+app.use( express.static('public'));
+
+
+//Lo ultimo problema rutas angular
+
+app.listen( 3000, () => {
+    console.log('Servidor corriendo en puerto ' + 3000);
+});
